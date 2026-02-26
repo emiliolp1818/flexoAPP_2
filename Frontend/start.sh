c25e61b322166b583756357bfb6e08945777bc0d
emiliolp1818 <emiliojlp18@hotmail.com>
2026-02-26 11:35:30 -0500

    fix: hardcode port 8080 in nginx - remove envsubst and CRLF issues

--- Frontend/start.sh
+++ Frontend/start.sh
@@ -1,4 +1,2 @@
 #!/bin/sh
-# Sustituye $PORT en la config de nginx y arranca el servidor
-envsubst '$PORT' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
 exec nginx -g 'daemon off;'
